a8030b75b5ad7c1b309599189d667d66550d5730
DevThunder11 <phat0822942488@gmail.com>
2025-03-16 18:03:50 +0700

    analysis
    hello

--- Pages/Analysis/analysis.js
+++ Pages/Analysis/analysis.js
@@ -157,10 +157,46 @@ if (loggedInUserId) {
                         console.log(percent)
 
                         chartOptions.series[0].data.push(percent);
+
                         // graph
                         console.log(chartOptions.series[0].data)
                         ///End///
                     }
+                    //Average
+                    function calculateAverage(dataArray) {
+                      if (dataArray.length === 0) return 0; // Handle empty array case
+                  
+                      let sum = dataArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+                      let average = sum / dataArray.length;
+                      return average;
+                    }
+                    let averagePercent = calculateAverage(chartOptions.series[0].data);
+                    // console.log("Average Percent:", averagePercent);
+                    
+                    document.getElementById("average").innerText = `${averagePercent}%`;
+                    if (averagePercent >= 80) {
+                      document.getElementById("average").style.color = "#90D5FF";
+                      document.getElementById("standard").innerText = "ผลการเรียนยอดเยี่ยม";
+                      document.getElementById("analysis_text").innerText = "จากผลการทดสอบ ค่าเฉลี่ยของคุณอยู่ในช่วง 80-100% ซึ่งแสดงให้เห็นถึงความเป็นเลิศในการเข้าใจและประยุกต์ใช้เนื้อหาอย่างยอดเยี่ยม ขอแสดงความยินดีในความสำเร็จและความทุ่มเทที่คุณมีต่อการเรียนรู้ พร้อมทั้งขอให้คุณรักษามาตรฐานนี้ไว้และมองหาความท้าทายใหม่ ๆ เพื่อก้าวหน้าต่อไป";
+                    } else if (averagePercent >= 60) {
+                      document.getElementById("average").style.color = "#008000";
+                      document.getElementById("standard").innerText = "ผลการเรียนที่ดี";
+                      document.getElementById("analysis_text").innerText = "ผลการทดสอบที่ได้แสดงให้เห็นว่าค่าเฉลี่ยอยู่ในช่วง 60-80% ซึ่งบ่งบอกถึงความพยายามและความเข้าใจในเนื้อหาที่ดี คุณมีพื้นฐานที่มั่นคงและสามารถประยุกต์ใช้ความรู้ได้อย่างเหมาะสม ขอให้รักษามาตรฐานนี้ไว้และมองหาวิธีการพัฒนาเพิ่มเติมเพื่อเสริมสร้างความรู้ในระดับที่สูงขึ้น";
+                    } else if (averagePercent >= 40) {
+                      document.getElementById("average").style.color = "#FFFF00";
+                      document.getElementById("standard").innerText = "ผลการเรียนระดับปานกลาง";
+                      document.getElementById("analysis_text").innerText = "ค่าเฉลี่ยในช่วง 40-60%สะท้อนให้เห็นว่าคุณมีพื้นฐานความรู้ที่ค่อนข้างมั่นคง แต่ยังมีช่องว่างในการพัฒนาเพิ่มเติม ขอให้พยายามทบทวนเนื้อหาที่มีความซับซ้อนและปรับปรุงเทคนิคการเรียนรู้ เพื่อให้ผลการทดสอบในครั้งต่อไปมีแนวโน้มที่ดีขึ้น";
+                    } else if (averagePercent >= 20) {
+                      document.getElementById("average").style.color = "#FF5733";
+                      document.getElementById("standard").innerText = "ยังต้องปรับปรุงและพัฒนาความรู้เพิ่มเติม";
+                      document.getElementById("analysis_text").innerText = "ผลการทดสอบที่ได้แสดงให้เห็นว่าค่าเฉลี่ยอยู่ในช่วง 20-40% ซึ่งหมายความว่ายังมีพื้นที่ให้พัฒนาอย่างมากในด้านความเข้าใจเนื้อหา ขอให้คุณทบทวนและฝึกฝนในส่วนที่ยังไม่ชัดเจนอย่างต่อเนื่อง และไม่ลังเลที่จะขอคำแนะนำหรือความช่วยเหลือจากผู้เชี่ยวชาญเพื่อก้าวข้ามความท้าทายนี้";
+                    } else {
+                      document.getElementById("average").style.color = "#880808";
+                      document.getElementById("standard").innerText = "ความเข้าใจพื้นฐานยังต้องพัฒนาอย่างมาก";
+                      document.getElementById("analysis_text").innerText = "จากผลการทดสอบที่ได้ ค่าเฉลี่ยอยู่ในช่วง 0-20% ซึ่งบ่งชี้ว่ามีความท้าทายในการทำความเข้าใจเนื้อหาพื้นฐานในระดับที่ต้องได้รับการปรับปรุงอย่างเร่งด่วน ขอแนะนำให้ทบทวนและฝึกฝนเนื้อหาเบื้องต้นอย่างละเอียด พร้อมทั้งปรึกษาครูผู้สอนเพื่อกำหนดแนวทางพัฒนาที่เหมาะสม";
+                    }
+                    //Average
+                    
                     // generate graph
                     var lineChart = new ApexCharts(document.querySelector('#line-chart'), chartOptions);
                     lineChart.render();
