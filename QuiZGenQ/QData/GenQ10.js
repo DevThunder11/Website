d1ab67a30018e9d6711e4477b6cff8eacac242ae
DevThunder11 <phat0822942488@gmail.com>
2025-04-20 19:27:47 +0700

    GenQ10

---
+++ QuiZGenQ/QData/GenQ10.js
@@ -0,0 +1,81 @@
+import { AnsNotIn,getRandomInt,getRandomIntMN,ArrayEqual,ArrayNotIn } from "/QuiZGenQ/QData/GobalFunction.js";
+
+export function GenRandomQ10(){
+
+    let Ans = [];
+    let TF = [false,false,false,false]
+
+    const namelist = ["ชิ","วิน","สายฟ้า","โฟ","ต้นตาล","ภีม"]
+    const question_text = "ความเร็วขนาด u เมตร/วินาที มีค่าเท่าไหร่ในหน่วย กิโลเมตร/ชั่วโมง".split(" ")
+
+    // QuestionCode
+
+    let Name = namelist[getRandomInt((namelist.length)-1)]
+    let u = getRandomIntMN(1, 20) * 5
+
+    while (!Number.isInteger(u*3.6)) {
+        u = getRandomIntMN(1, 20) * 5;
+    }
+
+
+    for (let i = 0; i < question_text.length; i++) {
+        if (question_text[i] == "name"){
+            question_text[i] = Name
+        }
+        else if (question_text[i] == "u"){
+            question_text[i] = u
+        }
+
+    }
+
+    // Anscode
+
+    let result = (u*3.6)
+    let choice = [result]
+
+    // RandomAnsCode
+
+    let choiceAmount = 4
+
+    while (choice.length < choiceAmount) {
+        // Generate a random number to add or subtract from the correct answer
+        let randomOffset = getRandomIntMN(1, 10); // Random number between 1 and 5
+        let WrongChoice = choice[getRandomIntMN(0, choice.length - 1)] + (randomOffset * ((-1) ** getRandomIntMN(0, 1)));
+        
+        while (WrongChoice <= 0) {
+            randomOffset = getRandomIntMN(1, 10); // Regenerate random offset if necessary
+            WrongChoice = choice[getRandomIntMN(0, choice.length - 1)] + (randomOffset * ((-1) ** getRandomIntMN(0, 1)));
+        }
+    
+        if (AnsNotIn(choice, WrongChoice)) {
+            choice = [...choice, WrongChoice];
+        }
+    }
+
+    // PrintCode
+
+    let questionPrint = question_text.join(" ")
+
+    choice.sort()
+    
+    for (let j = 0; j < choice.length; j++) {
+
+        let textAns = ([j+1,choice[j]].join(") "))
+
+        if (choice[j] == result){
+            Ans = [...Ans,{ text: textAns, correct: true}]
+        }
+        else {
+            Ans = [...Ans,{ text: textAns, correct: false}]
+        }
+    }
+
+    let box = {
+        question: questionPrint,
+        answers: Ans,
+        template: "Q10"
+    };
+    // console.log(box) // check
+
+    return box ;
+}
